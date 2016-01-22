2f18fb6aa5230bb801c222f927fb5d49af5ae685
ChangChung Wei <totorojohnny@yahoo.com.tw>
2016-01-22 16:30:12 +0800

    Implement devicedereg command and add a new function named add_event refs #6

--- genius_bar/slack.py
+++ genius_bar/slack.py
@@ -22,7 +22,7 @@ class SlackManager(object):
 
     def dbSetting(self):
         # sqlalchemy DBSession
-        sqlalchemy_url  = self.settings['sqlalchemy_url']
+        sqlalchemy_url  = self.settings['sqlalchemy.url']
         engine = create_engine(sqlalchemy_url)
         DBSession.configure(bind=engine)
         Base.metadata.bind = engine
@@ -60,9 +60,9 @@ class SlackManager(object):
         try:
             self.session.flush()
             check_device = GeniusDeviceQuery.get_by_name(self.session, self.rqbody['text'])
-            if check_device == None: # device exist 
+            if check_device == None: # device not exist 
                 check_user = GeniusUserQuery.get_by_name(self.session, self.rqbody['user_name'])
-                if check_user == None: # user exist 
+                if check_user == None: # user not exist 
                     input_user = GeniusUser(user_name = self.rqbody['user_name'], 
                                             slack_user_id = self.rqbody['user_id'])
                     self.session.add(input_user)
@@ -72,28 +72,55 @@ class SlackManager(object):
                 input_device = GeniusDevice(device_name = self.rqbody['text'], holder_id = check_user.id)
                 self.session.add(input_device)
                 self.session.commit()
-                self.add_event
                 check_device = GeniusDeviceQuery.get_by_name(self.session, self.rqbody['text'])
                 message = "Device id: " + str(check_device.id) + "  Device name: " + check_device.device_name
                 logging.info(message)
-                rtn = {"text": "regist finisth!",
+                rtn = {"text": "Regist finisth!",
                                "attachments": [{"color" : "good" , "text" : message}]}
+                self.add_event("reg", check_device.id, check_user.id)
             else:
-                rtn = {"text": "Device is already registed!"}
-            return rtn
+                rtn = {"text": "Device is already registed! Maybe you need to regist with a new name"}
         except Exception as e:
             self.session.rollback()
             print(e)
-
         #payload = {"text": "test message"}
         #r = requests.post("https://hooks.slack.com/services/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", json=payload)
-        return "devicereg"
+        return rtn
     def devicedereg(self):
-
-        x = {"text": "meow devicedreg", "attachments": [{"color": "good", 
-                                                         "text" : "Partly cloudy today and tomorrow"}]}
-        return x
+        try:
+            self.session.flush()
+            check_device = GeniusDeviceQuery.get_by_name(self.session, self.rqbody['text'])
+            if check_device is not None: # device exist 
+                check_user = GeniusUserQuery.get_by_name(self.session, self.rqbody['user_name'])
+                if check_user == None: # user exist 
+                    input_user = GeniusUser(user_name = self.rqbody['user_name'], 
+                                            slack_user_id = self.rqbody['user_id'])
+                    self.session.add(input_user)
+                    self.session.commit()
+                    check_user = GeniusUserQuery.get_by_name(self.session, self.rqbody['user_name'])
+                check_device.delete = True 
+                self.session.commit()
+                message = "Device id: " + str(check_device.id) + "  Device name: " + check_device.device_name
+                logging.info(message)
+                rtn = {"text": "Deregist finisth!",
+                               "attachments": [{"color" : "good" , "text" : message}]}
+                self.add_event("dereg", check_device.id, check_user.id)
+            else:
+                rtn = {"text": "Device not exist!"}
+        except Exception as e:
+            self.session.rollback()
+            print(e)
+        return rtn
     def deviceaudit():
+
         return "deviceaudit"
-    def add_event():
-        return "add_event"
+    def add_event(self, event_type, event_device_id, event_user_id):
+        try:
+            self.session.flush()
+            if event_type == "reg" or event_type == "dereg":
+                input_event = GeniusEvent(event_type = event_type, device_id = event_device_id, user_id = event_user_id)
+            self.session.add(input_event)
+            self.session.commit()
+        except Exception as e:
+            self.session.rollback()
+            logging.info(e)
